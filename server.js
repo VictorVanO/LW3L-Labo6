011f6721b2e48986698a96a72a046361e9ff84e5
VictorVanO <20253@ecam.be>
2022-12-12 01:16:48 +0100

    main view

--- server.js
+++ server.js
@@ -5,20 +5,31 @@
 let express = require('express');
 let app = express(); // Instancier le serveur
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 
-var mysql = require("mysql");
 // Database connection
+var mysql = require("mysql");
 var connection = mysql.createConnection({
     host        : 'localhost',
     user        : 'root',
     password    : 'root',
-    database    : 'tasks'
+    database    : 'formations'
 });
 connection.connect(function(error) {if (error) console.log(error);});
 
-app.use(express.static('public'));
+
 // Launch server
 app.listen(3000, function() {
     console.log('Server is running on port 3000');
 });
+
+
+// Set route
+// Méthode HTTP 'get' dont on déclare le chemin et 2 paramètres
+app.get('/', (req, res) => {
+    connection.query("select * from formation;", function(error, result) {
+        if(error) console.log(error);
+        res.render('Labo6.ejs', {formations: result});
+    });
+}); 
