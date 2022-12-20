096a7c9413dff525ef96f8a870fb44914ae4e00d
VictorVanO <20253@ecam.be>
2022-12-20 17:39:43 +0100

    SQL update 3

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 //! Pour le terminal : Terminal -> New Terminal -> Labo 6
 //! Lancer le serveur avec "npm run serve" pour lancer le serveur avec nodemon
 
+//Pour régler le problème avec mysql ASCII error : Utiliser la commande "mysqldump -u root -p formations -r academy.sql"
+//Ensutie sur ma VM, utiliser "sudo mysql -u root -p --default-character-set=utf8 academy"
+//Puis écrire dans <mysql> les commandes suivantes : 
+//mysql> SET names 'utf8';
+//mysql> SOURCE academy.sql
+
 // Start server with express
 let express = require('express');
 let app = express(); // Instancier le serveur
@@ -36,8 +42,6 @@ app.get('/', (req, res) => {
     });
 }); 
 
-let routes = require('./routes');
-app.use('/', routes);
 
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
@@ -63,6 +67,9 @@ app.get('/logout', function (req, res) {
     res.send('Logout success !');
 });
 
+
+let routes = require('./routes');
+app.use('/', routes);
 
 var port = process.env.PORT || 3000;
 // Launch app to listen to specified port
